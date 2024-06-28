f6a0aa53e5d1b6ba96fc7ff74479daf4da346036
Timur_2101 <timurka260197@gmil.com>
2024-06-28 09:11:15 +0200

    Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/Model/Emploee.java,5\5\55e8473bf874c75c401f4fad9d4f17095394488c
+Q
+!src/main/java/Model/Emploeee.java,7\7\7799f0828428df4eb2ac02d28dbd9d6fb5050c59
+G
+src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
+Q
+!src/main/java/Model/Employee.java,7\0\70ab5299a3e52c9007815ee13f20cdba8c678493
+P
+ src/main/java/Model/Manager.java,0\a\0acbc7baa09ee79c5598bb55959e7e6e5ffa0a48
---
+++ src/main/java/Model/Employee.java
@@ -0,0 +1,65 @@
+package Model;
+
+import lombok.Data;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+public class Employee implements Comparable<Employee> {
+    private String name;
+
+    private String secondName;
+    private String surName;
+    private String age;
+    private String position;
+    private String phoneNumber;
+    private int salary;
+
+
+    public Employee(String name, String secondName, String surName, String age, String position, String phoneNumber, int salary) {
+        this.name = name;
+        this.secondName = secondName;
+        this.surName = surName;
+        this.age = age;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+    }
+
+    public String getAllInfo() {
+         return ("Employee{" +
+                "name='" + name + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", surName='" + surName + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", salary=" + salary +
+                '}' );
+    }
+
+
+    public Date convertToDate(String date) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        return format.parse(date);
+    }
+
+//    public static void upSalary(List<Employee> employeeList , int procentUp) {
+//        for (Employee employee : employeeList) {
+//            employee.setSalary(employee.getSalary() +((employee.getSalary() * procentUp)) / 100);
+//        }
+//    }
+
+    @Override
+    public int compareTo(Employee o) {
+        try {
+            return (this.convertToDate(this.getAge()).getTime() > (o.convertToDate(o.getAge())).getTime()) ? 1 : -1;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
